753296e5f35d2f68ebcb8a9cfd776798a563ae85
danielfeisp <daniel.torres.geraldi@gmail.com>
2026-03-12 18:58:40 -0300

    Update exc12s1.py

--- Fund Algoritmos/exc12s1.py
+++ Fund Algoritmos/exc12s1.py
@@ -1,10 +1,12 @@
+import time
 def fatorial(numero_paia):
     f = 1
     for i in range (1,numero_paia+1):
         f = f*i
     return f
-
 x = int(input("Digite um número: "))
 while x!=0:
     print(fatorial(x))
     x = int(input("Digite um número: "))
+time.sleep(3)
+# Determina o resultado de fatorial de um número
